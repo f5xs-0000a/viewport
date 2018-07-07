831089ec5a61e6170d8f5af98a2bf32317e03182
F5XS <f5xs.0000a@gmail.com>
2018-07-07 19:25:20 +0900

    Changed a lot of stuff.

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,10 @@ extern crate nalgebra;
 
 use nalgebra as na;
 
+use na::Matrix3;
 use na::Matrix2;
 use na::Vector2;
+use na::Vector3;
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -61,7 +63,7 @@ impl CoordinateSystem {
     pub fn io_transform_matrix(
         &self,
         size: &(f64, f64),
-    ) -> Matrix2<f64> {
+    ) -> Matrix3<f64> {
         use CoordinateDominance::*;
         use CoordinateScale::*;
 
@@ -70,18 +72,22 @@ impl CoordinateSystem {
         
         // set everything to square
         let transform_to_square = match self.scale {
-            Square => Matrix2::identity(),
+            Square => Matrix3::identity(),
             Scale => match (&self.dominance, size.0, size.1) {
-                (WidthNormalized, x, y) => Matrix2::new(1., 0.,
-                                                        0., y / x),
-                (MaxNormalized, x, y) if x > y => Matrix2::new(1., 0.,
-                                                               0., y / x),
-                (MinNormalized, x, y) if x < y => Matrix2::new(1., 0.,
-                                                               0., y / x),
+                (WidthNormalized, x, y) => Matrix3::new(1., 0., 0.,
+                                                        0., y / x, 0.,
+                                                        0., 0., 1.),
+                (MaxNormalized, x, y) if x > y => Matrix3::new(1., 0., 0.,
+                                                               0., y / x, 0.,
+                                                               0., 0., 1.),
+                (MinNormalized, x, y) if x < y => Matrix3::new(1., 0., 0.,
+                                                               0., y / x, 0.,
+                                                               0., 0., 1.),
 
                 // too lazy to do the other condition
-                | (_, x, y) => Matrix2::new(x / y, 0.,
-                                            0.   , 1.),
+                | (_, x, y) => Matrix3::new(x / y, 0., 0.,
+                                            0.   , 1., 0.,
+                                            0., 0., 1.),
             }
         };
 
@@ -132,40 +138,49 @@ impl CoordinateSystem {
             }
         }
 
-        fn transform_to_center(origin: &CoordinateOrigin) -> Matrix2<f64>
-        {
-            let mut indom_to_center = Matrix2::identity();
+        fn transform_to_center(origin: &CoordinateOrigin) -> Matrix3<f64> {
+            let mut affine = Matrix3::identity();
 
             if flip_y(origin) {
-                indom_to_center *= Matrix2::new(1., 0.,
-                                                0., -1.);
+                affine = Matrix3::new(1., 0., 0.,
+                                      0., -1., 0.,
+                                      0., 0., 1.)
+                    * affine;
             }
 
             if flip_x(origin) {
-                indom_to_center *= Matrix2::new(-1., 0.,
-                                                0., 1.);
+                affine = Matrix3::new(-1., 0., 0.,
+                                      0., 1., 0.,
+                                      0., 0., 1.)
+                    * affine;
             }
 
             if scale_half(origin) {
-                indom_to_center *= Matrix2::new(0.5, 0.,
-                                                0., 0.5);
+                affine = Matrix3::new(0.5, 0., 0.,
+                                      0., 0.5, 0.,
+                                      0., 0., 1.)
+                    * affine;
             }
 
             if let Some(x_offset) = x_offset(origin) {
-                let x_offset = if x_offset { 1. } else { -1. };
+                let x_offset = if x_offset { 0.5 } else { -0.5 };
 
-                indom_to_center *= Matrix2::new(x_offset, 0.,
-                                                0., 0.);
+                affine = Matrix3::new(1., 0., x_offset,
+                                      0., 1., 0.,
+                                      0., 0., 1.)
+                    * affine;
             }
 
             if let Some(y_offset) = y_offset(origin) {
-                let y_offset = if y_offset { 1. } else { -1. };
+                let y_offset = if y_offset { 0.5 } else { -0.5 };
 
-                indom_to_center *= Matrix2::new(0., 0.,
-                                                0., y_offset);
+                affine = Matrix3::new(1., 0., 0.,
+                                      0., 1., y_offset,
+                                      0., 0., 1.)
+                    * affine;
             }
 
-            indom_to_center
+            affine
         };
 
         let center_to_transform = transform_to_center(&self.output_origin)
@@ -173,33 +188,62 @@ impl CoordinateSystem {
             .unwrap();
         let transform_to_center = transform_to_center(&self.input_origin);
 
-        transform_to_square * transform_to_center * center_to_transform
+        let dominant_length = match &self.dominance {
+            WidthDominant => size.0,
+            HeightDominant => size.1,
+            MaxDominant => {
+                if size.0 > size.1 {
+                    size.0
+                }
+
+                else {
+                    size.1
+                }
+            },
+
+            MinDominant => {
+                if size.0 < size.1 {
+                    size.0
+                }
+
+                else {
+                    size.1
+                }
+            },
+        };
+
+        Matrix3::new(dominant_length / 2., 0., 0.,
+                     0., dominant_length / 2., 0.,
+                     0., 0., 1.)
+            * transform_to_square
+            * transform_to_center
+            * center_to_transform
 
     }
 
     pub fn into_y(
         &self,
         input: (f64, f64),
-        size: (f64, f64),
+        size: &(f64, f64),
     ) -> (f64, f64) {
-        let transform_matrix = self.io_transform_matrix(&size);
-        let vector = transform_matrix * Vector2::new(input.0, input.1);
+        let transform_matrix = self.io_transform_matrix(size);
+        let vector = transform_matrix * Vector3::new(input.0, input.1, 0.);
 
-        (vector[(0, 0)], vector[(0, 1)])
+        (vector[(0, 0)], vector[(1, 0)])
     }
 
     pub fn into_x(
         &self,
         output: (f64, f64),
-        size: (f64, f64),
+        size: &(f64, f64),
     ) -> (f64, f64) {
         let transform_matrix = self
-            .io_transform_matrix(&size)
+            .io_transform_matrix(size)
             .try_inverse()
             .unwrap();
 
-        let vector = transform_matrix * Vector2::new(output.0, output.1);
+        let vector = transform_matrix * Vector3::new(output.0, output.1, 0.);
 
-        (vector[(0, 0)], vector[(0, 1)])
+        (vector[(0, 0)], vector[(1, 0)])
     }
 }
