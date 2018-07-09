bc435a9c9695c895d6490ecca300ee50b0f1a262
F5XS <f5xs.0000a@gmail.com>
2018-07-09 20:03:23 +0900

    Code finally works.

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,11 @@ extern crate nalgebra;
 use nalgebra as na;
 
 use na::Matrix3;
-use na::Matrix2;
-use na::Vector2;
 use na::Vector3;
 
 ////////////////////////////////////////////////////////////////////////////////
 
+#[derive(Eq, PartialEq, Hash, Debug, Copy, Clone)]
 pub enum CoordinateOrigin {
     /// zero at the center, positive towards top-right
     Center,
@@ -26,6 +25,7 @@ pub enum CoordinateOrigin {
     BottomRight,
 }
 
+#[derive(Eq, PartialEq, Hash, Debug, Copy, Clone)]
 pub enum CoordinateScale {
     /// unit interval in the x-axis is equidistant to the unit interval in the
     /// y-axis, thus a rectangular viewport may not have a complete unit
@@ -38,6 +38,7 @@ pub enum CoordinateScale {
     Scale,
 }
 
+#[derive(Eq, PartialEq, Debug, Clone)]
 pub enum CoordinateDominance {
     /// the width of the viewport will have a unit distance
     WidthNormalized,
@@ -52,6 +53,7 @@ pub enum CoordinateDominance {
     MinNormalized,
 }
 
+#[derive(Eq, PartialEq)]
 pub struct CoordinateSystem {
     pub scale: CoordinateScale,
     pub dominance: CoordinateDominance,
@@ -59,30 +61,31 @@ pub struct CoordinateSystem {
     pub output_origin: CoordinateOrigin,
 }
 
+use CoordinateScale as CS;
+use CoordinateOrigin as CO;
+use CoordinateDominance as CD;
+
 impl CoordinateSystem {
     pub fn io_transform_matrix(
         &self,
         size: &(f64, f64),
     ) -> Matrix3<f64> {
-        use CoordinateDominance::*;
-        use CoordinateScale::*;
-
         // okay, don't worry about the manual mathematics of these. hopefully,
         // the compiler would be smart enough to optimize this whole function.
         
         // set everything to square
         let transform_to_square = match self.scale {
-            Square => Matrix3::identity(),
-            Scale => match (&self.dominance, size.0, size.1) {
-                (WidthNormalized, x, y) => Matrix3::new(1., 0., 0.,
-                                                        0., y / x, 0.,
-                                                        0., 0., 1.),
-                (MaxNormalized, x, y) if x > y => Matrix3::new(1., 0., 0.,
-                                                               0., y / x, 0.,
-                                                               0., 0., 1.),
-                (MinNormalized, x, y) if x < y => Matrix3::new(1., 0., 0.,
-                                                               0., y / x, 0.,
-                                                               0., 0., 1.),
+            CS::Square => Matrix3::identity(),
+            CS::Scale => match (&self.dominance, size.0, size.1) {
+                (CD::WidthNormalized, x, y) => Matrix3::new(1., 0., 0.,
+                                                            0., y / x, 0.,
+                                                            0., 0., 1.),
+                (CD::MaxNormalized, x, y) if x > y => Matrix3::new(1., 0., 0.,
+                                                                   0., y / x, 0.,
+                                                                   0., 0., 1.),
+                (CD::MinNormalized, x, y) if x < y => Matrix3::new(1., 0., 0.,
+                                                                   0., y / x, 0.,
+                                                                   0., 0., 1.),
 
                 // too lazy to do the other condition
                 | (_, x, y) => Matrix3::new(x / y, 0., 0.,
@@ -92,54 +95,28 @@ impl CoordinateSystem {
         };
 
         fn flip_y(origin: &CoordinateOrigin) -> bool {
-            use CoordinateOrigin::*;
-
             match origin {
-                TopLeft | TopRight => true,
+                CO::TopLeft | CO::TopRight => true,
                 _ => false,
             }
         }
 
         fn flip_x(origin: &CoordinateOrigin) -> bool {
-            use CoordinateOrigin::*;
-
             match origin {
-                TopRight | BottomRight => true,
+                CO::TopRight | CO::BottomRight => true,
                 _ => false,
             }
         };
 
-        fn scale_half(origin: &CoordinateOrigin) -> bool {
-            use CoordinateOrigin::*;
-
-            match origin {
-                Center => false,
-                _ => true,
-            }
-        }
-
-        fn x_offset(origin: &CoordinateOrigin) -> Option<bool> {
-            use CoordinateOrigin::*;
-
-            match origin {
-                TopLeft | BottomLeft => Some(true),
-                TopRight | BottomRight => Some(false),
-                Center => None,
-            }
-        }
-
-        fn y_offset(origin: &CoordinateOrigin) -> Option<bool> {
-            use CoordinateOrigin::*;
-
-            match origin {
-                TopLeft | TopRight => Some(false),
-                BottomLeft | BottomRight => Some(true),
-                Center => None,
-            }
-        }
-
-        fn transform_to_center(origin: &CoordinateOrigin) -> Matrix3<f64> {
+        // this function returns the transformation matrix that will adjust
+        // the input coordinates, given the input origin specified, into center
+        //
+        // this function can also be used for transforming a center-fixed
+        // coordinate into some coordinate given the specified input origin, by
+        // inverting the returned matrix
+        fn center_to_transform(origin: &CoordinateOrigin) -> Matrix3<f64> {
             let mut affine = Matrix3::identity();
+            let not_center = *origin != CO::Center;
 
             if flip_y(origin) {
                 affine = Matrix3::new(1., 0., 0.,
@@ -147,51 +124,42 @@ impl CoordinateSystem {
                                       0., 0., 1.)
                     * affine;
             }
-
+            
             if flip_x(origin) {
                 affine = Matrix3::new(-1., 0., 0.,
                                       0., 1., 0.,
                                       0., 0., 1.)
                     * affine;
             }
-
-            if scale_half(origin) {
+            
+            if not_center {
+                // xy offset
+                affine = Matrix3::new(1., 0., 1.,
+                                      0., 1., 1.,
+                                      0., 0., 1.)
+                    * affine;
+                println!("{:?}", affine);
+                    
+                // scale half
                 affine = Matrix3::new(0.5, 0., 0.,
                                       0., 0.5, 0.,
                                       0., 0., 1.)
                     * affine;
             }
-
-            if let Some(x_offset) = x_offset(origin) {
-                let x_offset = if x_offset { 0.5 } else { -0.5 };
-
-                affine = Matrix3::new(1., 0., x_offset,
-                                      0., 1., 0.,
-                                      0., 0., 1.)
-                    * affine;
-            }
-
-            if let Some(y_offset) = y_offset(origin) {
-                let y_offset = if y_offset { 0.5 } else { -0.5 };
-
-                affine = Matrix3::new(1., 0., 0.,
-                                      0., 1., y_offset,
-                                      0., 0., 1.)
-                    * affine;
-            }
-
+            
             affine
         };
 
-        let center_to_transform = transform_to_center(&self.output_origin)
+        let transform_to_center = center_to_transform(&self.input_origin)
             .try_inverse()
             .unwrap();
-        let transform_to_center = transform_to_center(&self.input_origin);
+            
+        let center_to_transform = center_to_transform(&self.output_origin);
 
         let dominant_length = match &self.dominance {
-            WidthDominant => size.0,
-            HeightDominant => size.1,
-            MaxDominant => {
+            CD::WidthNormalized => size.0,
+            CD::HeightNormalized => size.1,
+            CD::MaxNormalized => {
                 if size.0 > size.1 {
                     size.0
                 }
@@ -201,7 +169,7 @@ impl CoordinateSystem {
                 }
             },
 
-            MinDominant => {
+            CD::MinNormalized => {
                 if size.0 < size.1 {
                     size.0
                 }
@@ -212,13 +180,12 @@ impl CoordinateSystem {
             },
         };
 
-        Matrix3::new(dominant_length / 2., 0., 0.,
-                     0., dominant_length / 2., 0.,
+        Matrix3::new(dominant_length, 0., 0.,
+                     0., dominant_length, 0.,
                      0., 0., 1.)
-            * transform_to_square
-            * transform_to_center
             * center_to_transform
-
+            * transform_to_center
+            * transform_to_square
     }
 
     pub fn into_y(
@@ -227,7 +194,7 @@ impl CoordinateSystem {
         size: &(f64, f64),
     ) -> (f64, f64) {
         let transform_matrix = self.io_transform_matrix(size);
-        let vector = transform_matrix * Vector3::new(input.0, input.1, 0.);
+        let vector = transform_matrix * Vector3::new(input.0, input.1, 1.);
 
         (vector[(0, 0)], vector[(1, 0)])
     }
